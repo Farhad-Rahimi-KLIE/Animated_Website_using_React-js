a53b8bfa34297766a30d94df0a493bc84e122f47
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-09 04:54:38 +0430

    part seven done

---
+++ Components/Services.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Card from "../Components/Card";
+import Sdata from '../Components/Sdata'
+
+const Services = () => {
+  return (
+    <div>
+        <div className="my-5">
+        <h1 className="text-center">Our Services</h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row">
+              {
+                Sdata.map((curElem,id)=>{
+                 return <Card key={id} imgsrc={curElem.imgsrc} title={curElem.title}/>
+                })
+              }
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Services
